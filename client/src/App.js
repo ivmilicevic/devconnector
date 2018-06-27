62b070f70c4e7dc88bea35cdbf29495849db1b2a
Ivan Milicevic <ivmilicevic1@gmail.com>
2018-06-27 20:19:42 +0200

    Created basic layout components, added react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// Import Components
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>My React app</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/test" component={Landing} />
+                    <Footer />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
