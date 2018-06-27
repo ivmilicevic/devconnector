30f95a0e2a2d3da3d66f63c70e1ed6492bcbdcb5
Ivan Milicevic <ivmilicevic1@gmail.com>
2018-06-27 20:31:47 +0200

    Initial react-router setup with placeholder components

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
 
 class App extends Component {
@@ -13,7 +16,11 @@ class App extends Component {
             <Router>
                 <div className="App">
                     <Navbar />
-                    <Route exact path="/test" component={Landing} />
+                    <Route exact path="/" component={Landing} />
+                    <div className="container">
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/login" component={Login} />
+                    </div>
                     <Footer />
                 </div>
             </Router>
