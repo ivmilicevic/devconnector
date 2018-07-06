20b4ce0af37e4bc796f8a3b6cd6a42ff1c41f188
Ivan Milicevic <ivmilicevic1@gmail.com>
2018-07-06 10:38:51 +0200

    Added actions and reducers for retrieving profile for currently logged in user

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 // Import Components
 import Navbar from './components/layout/Navbar';
@@ -12,8 +13,10 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
+import { CLEAR_CURRENT_PROFILE } from './actions/types';
 
 if (localStorage.jwtToken) {
     // Set request headers to jwt if there's saved token in localStorage
@@ -25,9 +28,10 @@ if (localStorage.jwtToken) {
 
     // Redirect user to login page if token has expired
     const currentTime = Date.now() / 1000;
-    if(decodedToken.exp < currentTime){
+    if (decodedToken.exp < currentTime) {
+        // Log user out and clear currenly loaded profile
         store.dispatch(logoutUser());
-        // TODO: Clear current profile
+        store.dispatch(clearCurrentProfile());
         // Redirect to login page
         window.location.href = '/login';
     }
@@ -44,6 +48,7 @@ class App extends Component {
                         <div className="container">
                             <Route exact path="/register" component={Register} />
                             <Route exact path="/login" component={Login} />
+                            <Route exact path="/dashboard" component={Dashboard} />
                         </div>
                         <Footer />
                     </div>
