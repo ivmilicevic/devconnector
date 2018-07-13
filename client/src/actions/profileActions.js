8a34a51455601e3244f359df23b7b5ee33ce0064
Ivan Milicevic <ivmilicevic1@gmail.com>
2018-07-13 13:56:35 +0200

    Added functionality for adding and deleting user education and experience

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -68,5 +68,32 @@ export const addEducation = (eduData, history) => dispatch => {
             type: GET_ERRORS,
             payload: err.response.data
         }))
+}
 
+export const deleteExperience = (id, ) => dispatch => {
+    axios.delete(`/api/profile/experience/${id}`)
+        .then(res => dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+        )
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+        )
+}
+
+export const deleteEducation = (id, ) => dispatch => {
+    axios.delete(`/api/profile/education/${id}`)
+        .then(res => dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+        )
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+        )
 } 
